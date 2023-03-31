66c0cbab9a894ef29af994b1f59ac6a96e98102e
Robbe Saelens <robbe.saelens@student.howest.be>
2023-03-31 14:57:01 +0200

    commit

--- src/screens/Projects.vue
+++ src/screens/Projects.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="animate__animated animate__backInDown mx-auto my-auto max-w-screen-2xl px-6">
-    <h1 class="my-10 text-4xl font-bold">My Projects</h1>
+  <h1 class="mx-auto my-auto mb-10 max-w-screen-2xl px-6 text-4xl font-bold">My Projects</h1>
+  <div
+    class="animate__animated animate__backInDown pb-50 mx-auto my-auto h-screen max-w-screen-2xl overflow-y-scroll px-6"
+  >
     <div class="grid grid-cols-1 gap-10 md:grid-cols-2">
       <div class="cursor-pointer overflow-hidden">
         <img
-          class="transition-300 opacity-90 hover:opacity-100"
+          class="h-100 transition-300 w-full object-cover opacity-90 hover:opacity-100"
           src="/mockup.jpg"
           alt="Mockup Bikerental"
         />
@@ -16,12 +18,36 @@
 
       <div class="cursor-pointer overflow-hidden">
         <img
-          class="transition-300 opacity-90 hover:opacity-100"
+          class="h-100 transition-300 w-full object-cover opacity-90 hover:opacity-100"
           src="/mockup.jpg"
           alt="Mockup Bikerental"
         />
         <div class="pt-4">
-          <h2 class="text-xl font-bold text-teal-900">BikeRental Robbe</h2>
+          <h2 class="text-xl font-bold text-teal-900">Binance smartwatch app</h2>
+          <p class="mb-5 text-sm text-gray-500">#UI_DESIGN</p>
+        </div>
+      </div>
+
+      <div class="cursor-pointer overflow-hidden">
+        <img
+          class="h-100 transition-300 w-full object-cover opacity-90 hover:opacity-100"
+          src="/ResearchMockup.jpg"
+          alt="Mockup Bikerental"
+        />
+        <div class="pt-4">
+          <h2 class="text-xl font-bold text-teal-900">Netflix SaaS</h2>
+          <p class="mb-5 text-sm text-gray-500">#WEB_DEVELOPMENT</p>
+        </div>
+      </div>
+
+      <div class="cursor-pointer overflow-hidden">
+        <img
+          class="h-100 transition-300 w-full object-cover opacity-90 hover:opacity-100"
+          src="/Vital.jpg"
+          alt="Mockup Bikerental"
+        />
+        <div class="pt-4">
+          <h2 class="text-xl font-bold text-teal-900">Vital Cities Beweegscan</h2>
           <p class="mb-5 text-sm text-gray-500">#WEB_DEVELOPMENT</p>
         </div>
       </div>
